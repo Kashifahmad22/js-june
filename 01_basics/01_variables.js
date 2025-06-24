0baafd579836e8e4dacf7cc1b395e7dd7722008e
Kashifahmad22 <mkkashif2002@gmail.com>
2025-06-24 08:43:33 +0000

    config

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId =144553
+let accountEmail="mkkashif2002@gmail.com"
+var accountPassword="123456789"
+accountCity ="Begusarai"
+let accountState ;
+//accountId=2
+
+accountEmail="kashifbaba@gmail.com"
+accountPassword="4664645"
+accountCity="patna"
+
+
+//This is not allowed
+console.log(accountId);
+
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope
+
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all js code as newer version
+
+
+//alert("Hello") // we are using node.js not the  browser
+
+console.log(3+3);
+
+//code readability should be high and to be more focused upon
+
+console.log("kashif")
+
+let name="kashif" //string
+let age =18 //int
+let isloggedIn =false // boolean 
+let state;
+
+//numer => 2 to the power 53
+//bigint 
+//string ""
+//boolean => true
+//null => standalone value
+//undefined => variabled declarred but value not assigned
+//symbol =>unique
+
+//object 
+
+console.log(typeof undefinied)//undefined
+console.log(typeof null) //object
