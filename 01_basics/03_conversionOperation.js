ffc2f1af19f44cf8c22d9d0b4eee002dd8d87443
Kashifahmad22 <mkkashif2002@gmail.com>
2025-06-24 11:58:00 +0000

    config

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,8 +24,66 @@
     //"" => false
     //"kashif" => true
 
-    let someNumber =33
+    /*let someNumber =33
 
     let stringNumber = String(someNumber)
     console.log(stringNumber)
-     console.log(typeof stringNumber)
+     console.log(typeof stringNumber)*/
+
+
+//***********************Operations*********************************
+let value=3
+let negValue=-value
+console.log(negValue)
+/*
+console.log(2+2);
+console.log(2*2);
+console.log(2/2);
+console.log(2%3);
+
+let str1="hello"
+let str2=" kashif"
+ 
+let str3= str1+ str2;
+
+console.log(str3)*/
+
+/*console.log("2"+2+2);
+console.log(2+"2"+2);
+console.log("2"+"2"+"2");
+console.log(2+3+"1");
+console.log(2+"3"+"1");  //first value is a integer*/ 
+
+console.log(true);
+
+
+let num1,num2,num3
+
+num1=num2=num3= 2 + 2
+
+let gameCounter =100
+++gameCounter;
+console.log(gameCounter);
+
+
+
+//JavaScript Demo: Increment (++) operator
+
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+//  //The idea is y=x(++) i.e. y is first using x then incrementing 
+//since now y has incremented so x==4
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+//  //The idea is b=(++)3 i.e. b is first incrementing a then using a 
+//since now a has incremented so a==4
+//till lecture seven
+
