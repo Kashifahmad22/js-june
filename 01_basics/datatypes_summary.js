c4cc8886c871f54c8fdbe7a90b704d97afcf8bcb
Kashifahmad22 <mkkashif2002@gmail.com>
2025-06-24 18:03:17 +0000

    config

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,39 @@
+// Primitive Datatypes
+
+
+// 7 types : String ,Numer, Boolean, null ,undefined,symbol,BigInt
+
+/*const score= 100;
+const scoreValue= 100.3;
+
+const isLoggedIn = false
+const outsideTemp= null
+ 
+let userEmail = undefined
+
+const id = Symbol("123")
+const anotherId =Symbol("123")
+
+console.log(id == anotherId);*/
+
+//const bigNumber=46464669464867486n
+
+// Reference (Non Primitive) 
+
+// Array, Objects, Functions
+
+
+const heros =["shaktiman","naagraj","doga"]
+letmyObj ={
+    name:"hitesh",
+    age:22
+
+
+}
+
+const myFunction=function(){
+    console.log("HelloWorld")
+
+}
+
+console.log(typeof myFunction);
