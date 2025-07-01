5d5a323d340c186ece501e7d75190c24c33aaeda
Kashifahmad22 <mkkashif2002@gmail.com>
2025-07-01 18:28:11 +0000

    config

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,34 @@
+//DATES
+
+
+let myDate =new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleDateString());
+// console.log(typeof myDate);
+
+
+// let myCreatedDate =new Date(2023,0,23)
+// let myCreatedDate =new Date(2023,0,23,5,3)
+// let myCreatedDate =new Date("2023-01-14")
+let myCreatedDate =new Date("01-14-2023")
+console.log(myCreatedDate.toLocaleString());
+
+
+let myTimeStamp = Date.now()
+console.log(myCreatedDate.getTime());
+console.log(myTimeStamp);
+console.log(Math.floor(Date.now()/1000));
+
+let newDate =new Date()
+console.log(newDate.getMonth());
+
+console.log(`Today is ${newDate.getDay()} and the time is ${Date.now()} ` )
+
+
+newDate.toLocaleDateString('default',{
+
+weekday:"long"
+// timeZone:''
+
+})
