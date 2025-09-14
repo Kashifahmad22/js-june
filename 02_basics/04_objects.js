5f9758df2331cc3e808f596cc04d76e08b33991a
Kashifahmad22 <mkkashif2002@gmail.com>
2025-09-14 16:05:15 +0000

    config

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,57 @@
+//const tinderUser= new Object(); //singleton object 
+const tinderUser ={} //non-snigleton object
+
+tinderUser.id="123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+
+
+// console.log(tinderUser);    
+const regularUser ={
+    email:"regularUser@example.com",
+    fullname:{
+        userFullname:{
+            firstname:"kashif",
+            lastname:"ahmed"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userFullname.firstname);
+
+// const obj1 = {1:"a",2:"b"}
+// const obj2 = {3:"c",4:"d"}
+
+// const obj3 ={obj1 ,obj2} //nested object    
+// const obj3 = Object.assign({},obj1,obj2 )      //merging objects
+
+ //const obj3 = {...obj1,...obj2} //spread operator
+
+
+//console.log(obj3);  
+const users =[
+    {
+        id:1,
+        email:"h@gmail.com"
+    }, {
+        id:1,
+        email:"h@gmail.com"
+    }, {
+        id:1,
+        email:"h@gmail.com"
+    },
+
+
+]
+users[1].email
+console.log(tinderUser);
+
+ console.log(Object.keys(tinderUser));//array of keys
+ console.log(Object.values(tinderUser));      //array of values
+ console.log(Object.entries(tinderUser)); //array of arrays     
+
+console.log(tinderUser.hasOwnProperty("name")); //true
+console.log(tinderUser.hasOwnProperty("isLoggedIn")); //true
+console.log(tinderUser.hasOwnProperty("email"));         //false    
+
