eceaa82c8ccad6d400d642f08ac9aac2bc322a20
Kashifahmad22 <mkkashif2002@gmail.com>
2025-09-15 15:12:09 +0000

    config

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -31,13 +31,44 @@
 
 
 // function loginUserMessage(username){
-function loginUserMessage(username ="KASHIF"){ //if loop me jaega hi ni kabhii bhii
-    if(username === undefined){
-        console.log("Please enter a username");
-        return  
-    }
-    return `${username} just logged in`
-}
-loginUserMessage("kashif")  
+// function loginUserMessage(username ="KASHIF"){ //if loop me jaega hi ni kabhii bhii
+//     if(username === undefined){
+//         console.log("Please enter a username");
+//         return  
+//     }
+//     return `${username} just logged in`
+//          }
+// loginUserMessage("kashif")  
 // console.log(loginUserMessage("kashif"));
-console.log(loginUserMessage("ARAFAT"));
+// console.log(loginUserMessage("ARAFAT"));
+
+
+function calculateCartPrice(val1,val2,...num1){  //rest operator
+return num1
+}
+
+//console.log(calculateCartPrice(200,400,500,2000));
+
+const user = {
+    username: "Kashif",
+    price: 199,
+    
+}
+ function handleObject(anyObject){
+console.log(`Username is ${anyObject.username} and the price is ${anyObject.price}`);
+
+ }
+ //handleObject(user)
+ handleObject({ 
+    username:"kashif",
+    price: 1999
+ })
+
+ const myNewArray = [200,400,100,600]
+ function returnSecondValue(getArray){
+    return getArray[1]
+ }
+console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([1000,2000,3000,4000]));
+
+
